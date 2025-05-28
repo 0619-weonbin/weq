e33d01e8dbb66b086ebfd6a8319337ce5b6b45f7
Yunseo <ys040202@naver.com>
2025-05-29 00:13:01 +0900

    first commit

---
+++ app/src/main/java/com/example/project/DBHelper.java
@@ -0,0 +1,28 @@
+package com.example.project;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "MyDatabase.db";
+    private static final int DB_VERSION = 1;
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS clothes (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "style TEXT, type TEXT, detailed_type TEXT, preference INTEGER, image_url TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS clothes");
+        onCreate(db);
+    }
+}
+
