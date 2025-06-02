338ac2da1a63b900a9bf30880847fe7ddb310bff
corykim2 <sos2591@naver.com>
2025-06-02 20:47:07 +0900

    이식 완료

--- app/src/main/java/com/example/myapplication123/MainActivity.java
+++ app/src/main/java/com/example/myapplication123/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication123;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,6 +40,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goToClosetBtn = (Button)findViewById(R.id.goToClosetButton);
+        goToClosetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Closet.class);
+                startActivity(intent);
+                Log.d("Closet", "옷장 버튼 클릭됨"); // 로그 추가 (확인용)
+            }
+        });
 
         RecyclerView hourlyWeatherRecyclerView = findViewById(R.id.hourlyWeatherRecyclerView);
         hourlyWeatherRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
